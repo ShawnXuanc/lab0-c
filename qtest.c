8d3be5a0f57be59e5a38484a0942b101c7e79736
shawn <ddoof1231@gmail.com>
2024-03-27 16:21:06 +0800

    Integrating the homework3 into lab0-c
    Add the contents related to MCTS in the ttt project to the lab0-c,
    and modify the Makefile to compile the newly added files.
    Also, update the pre-hook content to resolve the warnings issued
    by cppcheck so that commits can pass.
    Add hlist for zobrist.
    Handling the issue of using const variables for game and mt19937-64.

--- qtest.c
+++ qtest.c
@@ -41,7 +41,9 @@ extern int show_entropy;
  * OK as long as head field of queue_t structure is in first position in
  * solution code
  */
+#include "agents/mcts.h"
 #include "console.h"
+#include "game.h"
 #include "list_sort.h"
 #include "queue.h"
 #include "report.h"
@@ -1131,6 +1133,137 @@ static bool do_next(int argc, char *argv[])
     return q_show(0);
 }
 
+static int move_record[N_GRIDS];
+static int move_count = 0;
+
+static void record_move(int move)
+{
+    move_record[move_count++] = move;
+}
+
+static void print_moves()
+{
+    printf("Moves: ");
+    for (int i = 0; i < move_count; i++) {
+        printf("%c%d", 'A' + GET_COL(move_record[i]),
+               1 + GET_ROW(move_record[i]));
+        if (i < move_count - 1) {
+            printf(" -> ");
+        }
+    }
+    printf("\n");
+}
+
+static int get_input(char player)
+{
+    char *line = NULL;
+    size_t line_length = 0;
+    int parseX = 1;
+
+    int x = -1, y = -1;
+    while (x < 0 || x > (BOARD_SIZE - 1) || y < 0 || y > (BOARD_SIZE - 1)) {
+        printf("%c> ", player);
+        int r = getline(&line, &line_length, stdin);
+        if (r == -1)
+            exit(1);
+        if (r < 2)
+            continue;
+        x = 0;
+        y = 0;
+        parseX = 1;
+        for (int i = 0; i < (r - 1); i++) {
+            if (isalpha(line[i]) && parseX) {
+                x = x * 26 + (tolower(line[i]) - 'a' + 1);
+                if (x > BOARD_SIZE) {
+                    // could be any value in [BOARD_SIZE + 1, INT_MAX]
+                    x = BOARD_SIZE + 1;
+                    printf("Invalid operation: index exceeds board size\n");
+                    break;
+                }
+                continue;
+            }
+            // input does not have leading alphabets
+            if (x == 0) {
+                printf("Invalid operation: No leading alphabet\n");
+                y = 0;
+                break;
+            }
+            parseX = 0;
+            if (isdigit(line[i])) {
+                y = y * 10 + line[i] - '0';
+                if (y > BOARD_SIZE) {
+                    // could be any value in [BOARD_SIZE + 1, INT_MAX]
+                    y = BOARD_SIZE + 1;
+                    printf("Invalid operation: index exceeds board size\n");
+                    break;
+                }
+                continue;
+            }
+            // any other character is invalid
+            // any non-digit char during digit parsing is invalid
+            // TODO: Error message could be better by separating these two cases
+            printf("Invalid operation\n");
+            x = y = 0;
+            break;
+        }
+        x -= 1;
+        y -= 1;
+    }
+    free(line);
+    return GET_INDEX(y, x);
+}
+
+void mtcs_ttt()
+{
+    srand(time(NULL));
+    char table[N_GRIDS];
+    memset(table, ' ', N_GRIDS);
+    char turn = 'X';
+    char ai = 'O';
+
+    while (1) {
+        char win = check_win(table);
+        if (win == 'D') {
+            draw_board(table);
+            printf("It is a draw!\n");
+            break;
+        } else if (win != ' ') {
+            draw_board(table);
+            printf("%c won!\n", win);
+            break;
+        }
+
+        if (turn == ai) {
+            int move = mcts(table, ai);
+            if (move != -1) {
+                table[move] = ai;
+                record_move(move);
+            }
+
+        } else {
+            draw_board(table);
+            int move;
+            while (1) {
+                move = get_input(turn);
+                if (table[move] == ' ') {
+                    break;
+                }
+                printf("Invalid operation: the position has been marked\n");
+            }
+            table[move] = turn;
+            record_move(move);
+        }
+        turn = turn == 'X' ? 'O' : 'X';
+    }
+    print_moves();
+}
+
+
+static bool do_ttt(int argc, char *argv[])
+{
+    mtcs_ttt();
+    return 0;
+}
 static void console_init()
 {
     ADD_COMMAND(new, "Create new queue", "");
@@ -1174,6 +1307,7 @@ static void console_init()
     ADD_COMMAND(shuffle, "Shuffle list", "");
     ADD_COMMAND(listsort, "Using the list_sort from linux kernel version ", "");
     ADD_COMMAND(timsort, "Using the timsort", "");
+    ADD_COMMAND(ttt, "Play 4x4 Tic-Tac-Toe game", "");
     add_param("length", &string_length, "Maximum length of displayed string",
               NULL);
     add_param("malloc", &fail_probability, "Malloc failure probability percent",
